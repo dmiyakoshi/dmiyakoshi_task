f7e9340a79c00c452b2f29b7275124b330684399
dmiyakoshi <d1m354dai@gmail.com>
2021-09-20 21:08:16 +0900

    タスク管理アプリ課題完了

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use App\Http\Requests\ItemRequest;
+use SebastianBergmann\Type\VoidType;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+
+        return view('tasks.index', ['tasks' => $tasks]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ItemRequest $request)
+    {
+        $task = new Task();
+
+        $task->title = $request->title;
+        $task->body = $request->body;
+
+        $task->save();
+
+        return redirect('/tasks');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $task = Task::find($id);
+
+        return view('tasks.show', ['task' => $task]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $task = Task::find($id);
+
+        return view('tasks.edit', ['task' => $task]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ItemRequest $request, $id)
+    {
+        $task = Task::find($id);
+
+        $task->title = $request->title;
+        $task->body = $request->body;
+
+        $task->save();
+
+        return redirect('/tasks');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+
+        return redirect('/tasks');
+    }
+}
